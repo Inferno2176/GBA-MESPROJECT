d7667e4da1a15d4f830b47408cabd2d8ee02330c
U-LAPTOP-GPUSVF6H\SHALOM <Shalom J>
2026-04-07 17:55:30 +0530

    Push all files as requested

---
+++ build/template.d
@@ -0,0 +1,17 @@
+template.o: C:/msys64/home/SHALOM/PokemonBlackNights/source/template.c \
+ C:/devkitpro/libgba/include/gba_interrupt.h \
+ C:/devkitpro/libgba/include/gba_base.h \
+ C:/devkitpro/libgba/include/gba_types.h \
+ C:/devkitpro/libgba/include/gba_systemcalls.h \
+ C:/devkitpro/libgba/include/gba_dma.h \
+ C:/msys64/home/SHALOM/PokemonBlackNights/source/tiles.h \
+ C:/msys64/home/SHALOM/PokemonBlackNights/source/townmap.h \
+ C:/msys64/home/SHALOM/PokemonBlackNights/source/player.h
+C:/devkitpro/libgba/include/gba_interrupt.h:
+C:/devkitpro/libgba/include/gba_base.h:
+C:/devkitpro/libgba/include/gba_types.h:
+C:/devkitpro/libgba/include/gba_systemcalls.h:
+C:/devkitpro/libgba/include/gba_dma.h:
+C:/msys64/home/SHALOM/PokemonBlackNights/source/tiles.h:
+C:/msys64/home/SHALOM/PokemonBlackNights/source/townmap.h:
+C:/msys64/home/SHALOM/PokemonBlackNights/source/player.h:
---
+++ build/tiles.d
@@ -0,0 +1,1 @@
+tiles.o: C:/msys64/home/SHALOM/PokemonBlackNights/source/tiles.c
